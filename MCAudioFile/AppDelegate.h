b77bcf01c5839a76f8f26e611134c694525b2e59
Mike Ching <msching@163.com>
2014-08-10 15:43:59 +0800

    upload code

---
+++ MCAudioFile/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MCAudioFile
+//
+//  Created by Chengyin on 14-8-10.
+//  Copyright (c) 2014年 Chengyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
